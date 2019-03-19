b2f820ff2dcaa5fb445e72286351b3f7a2ddbf79
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2019-03-19 14:43:22 -0400

    Creando DAL, instalando entityframework

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+        public DbSet<Renta> Renta { get; set; }
+        public DbSet<RentasDetalle> Rentadetalle { get; set; }
+
+        public Contexto() : base("Constr") { }
+    }
+}
