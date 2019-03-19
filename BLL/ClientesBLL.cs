4b7104d731c886e8c98f7bf8e281ef493ae6863c
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2019-03-19 14:49:17 -0400

    Creando BLL

---
+++ BLL/ClientesBLL.cs
@@ -0,0 +1,95 @@
+﻿using DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ClientesBLL
+    {
+        public static bool Guardar(Clientes clientes)
+        {
+            Contexto db = new Contexto();
+            bool paso = false;
+            try
+            {
+                if (db.Clientes.Add(clientes) != null)
+                {
+                    db.SaveChanges();
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            { throw; }
+            finally
+            { db.Dispose(); }
+            return paso;
+        }
+        public static bool Modificar(Clientes clientes)
+        {
+            Contexto db = new Contexto();
+            bool paso = true;
+            try
+            {
+                db.Entry(clientes).State = System.Data.Entity.EntityState.Modified;
+                if (db.SaveChanges() > 0)
+                    paso = true;
+            }
+            catch (Exception)
+            { throw; }
+            finally
+            { db.Dispose(); }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            Contexto db = new Contexto();
+            bool paso = true;
+            try
+            {
+                Clientes clientes = db.Clientes.Find(id);
+                db.Clientes.Remove(clientes);
+                if (db.SaveChanges() > 0)
+                    paso = true;
+            }
+            catch (Exception)
+            { throw; }
+            finally
+            { db.Dispose(); }
+            return paso;
+        }
+        public static Clientes Buscar(int id)
+        {
+            Contexto db = new Contexto();
+            Clientes clientes = new Clientes();
+            try
+            {
+                clientes = db.Clientes.Find(id);
+            }
+            catch (Exception)
+            { throw; }
+            finally
+            { db.Dispose(); }
+            return clientes;
+        }
+        public static List<Clientes> GetList(Expression<Func<Clientes, bool>> expression)
+        {
+            Contexto db = new Contexto();
+            List<Clientes> clientes = new List<Clientes>();
+            try
+            {
+                clientes = db.Clientes.Where(expression).ToList();
+            }
+            catch (Exception)
+            { throw; }
+            finally
+            { db.Dispose(); }
+            return clientes;
+        }
+    }
+}
+
---
+++ BLL/RentaBLL.cs
@@ -0,0 +1,149 @@
+﻿using DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class RentaBLL
+    {
+        public static bool Guardar(Renta renta)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.Renta.Add(renta) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Renta renta)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var Anterior = db.Renta.Find(renta.RentaId);
+                db.Entry(renta).State = EntityState.Modified;
+                foreach (var item in Anterior.Detalle)
+                {
+                    /*if (!renta.Vehiculos.Exists(d => d.VehiculoID== item.VehiculoID))
+                        db.Entry(item).State = EntityState.Deleted;*/
+                    if (item.VehiculoId == 0)
+                        guardarDetalle(item);
+                    else
+                    {
+                        db.Entry(item).State = EntityState.Modified;
+                        paso = db.SaveChanges() > 0;
+                    }
+                }
+
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            { throw; }
+            finally
+            { db.Dispose(); }
+
+            return paso;
+        }
+
+        private static bool guardarDetalle(RentasDetalle Detalle)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.Rentadetalle.Add(Detalle) != null)
+                {
+                    db.SaveChanges();
+                    paso = true;
+                }
+            }
+            catch (Exception)
+            { throw; }
+            finally
+            { db.Dispose(); }
+            return paso;
+
+        }
+
+        public static bool Eliminar(int Id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var Eliminar = db.Renta.Find(Id);
+                db.Entry(Eliminar).State = System.Data.Entity.EntityState.Deleted;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Renta Buscar(int Id)
+        {
+            Contexto db = new Contexto();
+            Renta renta = new Renta();
+            try
+            {
+                renta = db.Renta.Find(Id);
+                renta.Detalle.Count();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return renta;
+        }
+
+
+        public static List<Renta> GetList(Expression<Func<Renta, bool>> renta)
+        {
+            List<Renta> Lista = new List<Renta>();
+            Contexto db = new Contexto();
+            try
+            {
+                Lista = db.Renta.Where(renta).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return Lista;
+        }
+
+    }
+}
