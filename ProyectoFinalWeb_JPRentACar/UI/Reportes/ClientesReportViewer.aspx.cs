3f8ae4b7f42b1ffa1f76d362c59f11a7d083027e
Oscar Slater Jimenez Paula <39197211+oscarslaterj@users.noreply.github.com>
2019-03-27 13:41:44 -0300

    Creando reporte de clientes y mejorando login

---
+++ ProyectoFinalWeb_JPRentACar/UI/Reportes/ClientesReportViewer.aspx.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Reporting.WebForms;
+using ProyectoFinalWeb_JPRentACar.UI.Consultas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoFinalWeb_JPRentACar.UI.Reportes
+{
+    public partial class ClientesReportViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ListadoReportViewer.ProcessingMode = ProcessingMode.Local;
+                ListadoReportViewer.Reset();
+                ListadoReportViewer.LocalReport.ReportPath = Server.MapPath(@"~\UI\Reportes\ClientesReport.rdlc");
+                ListadoReportViewer.LocalReport.DataSources.Clear();
+                ListadoReportViewer.LocalReport.DataSources.Add(new ReportDataSource("ClientesDataSet", cClientes.RetornarClientes()));
+                ListadoReportViewer.LocalReport.Refresh();
+            }
+        }
+    }
+}
